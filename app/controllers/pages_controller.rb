de51d17d62571864ed601a50dfca5ceec34b0ba4
Mathieu Longe <mathieu.longe@orange.fr>
2023-10-21 14:44:41 +1100

    Add about page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,8 +1,11 @@
 class PagesController < ApplicationController
-  skip_before_action :authenticate_user!, only: :home
+  skip_before_action :authenticate_user!, only: [:home, :about]
 
   def home
     @user = current_user
+  end
+
+  def about
   end
 
 end
